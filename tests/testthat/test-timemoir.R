bc0a2f26c7ea515ea8226f799d4ac0f05a304754
Nicolas Chuche <nicolas@barna.be>
2024-05-10 18:15:21 +0200

    fix tests for macos

--- tests/testthat/test-timemoir.R
+++ tests/testthat/test-timemoir.R
@@ -5,7 +5,7 @@ test_that("timemoir works", {
   expect_equal(colnames(result), c('fname', 'duration', 'error', 'start_mem', 'max_mem'))
 
   expect_equal(result$fname[[1]], "Sys.sleep(1)")
-  expect_lte(result$duration[[1]] - 1, 0.01)
+  expect_lte(result$duration[[1]] - 1, 0.2)
   expect_true(is.na(result$error[[1]]))
   expect_snapshot(result$error[[1]])
 
